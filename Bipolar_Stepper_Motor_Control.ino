e892c9cb45e051c7464836c16778b49a10775b50
Pradeshi <104403515+Pradeshi@users.noreply.github.com>
2023-01-09 11:00:14 +1100

    Added Joystick Functionality

--- Bipolar_Stepper_Motor_Control.ino
+++ Bipolar_Stepper_Motor_Control.ino
@@ -9,58 +9,65 @@
    A_bar / A / B_bar / B
    Black / Red / Yellow / Green
 */
+#include "PinChangeInterrupt.h";
 
 // for our up/down motor
-#define A1_bar    2                  // the pin connected to the wire A- of the coil A (or to the H-bridge pin controlling the same wire) 
-#define A1        3                  // the pin connected to the wire A of the coil A (or to the H-bridge pin controlling the same wire)
-#define B1_bar    4                  // the pin connected to the wire B- of the coil B (or to the H-bridge pin controlling the same wire)
-#define B1        5                  // the pin connected to the wire B of the coil B (or to the H-bridge pin controlling the same wire)
+#define A1motor_bar    A2                 // the pin connected to the wire A- of the coil A (or to the H-bridge pin controlling the same wire) 
+#define A1motor        A3                  // the pin connected to the wire A of the coil A (or to the H-bridge pin controlling the same wire)
+#define B1_bar    A4                  // the pin connected to the wire B- of the coil B (or to the H-bridge pin controlling the same wire)
+#define B1        A5                  // the pin connected to the wire B of the coil B (or to the H-bridge pin controlling the same wire)
 // for our left/right motor
-#define A2_bar    8                  // the pin connected to the wire A- of the coil A (or to the H-bridge pin controlling the same wire) 
-#define A2        9                  // the pin connected to the wire A of the coil A (or to the H-bridge pin controlling the same wire)
-#define B2_bar    10                 // the pin connected to the wire B- of the coil B (or to the H-bridge pin controlling the same wire)
-#define B2        11                 // the pin connected to the wire B of the coil B (or to the H-bridge pin controlling the same wire)
-// motor control variables
+#define A2motor_bar    9                  // the pin connected to the wire A- of the coil A (or to the H-bridge pin controlling the same wire) 
+#define A2motor        10                  // the pin connected to the wire A of the coil A (or to the H-bridge pin controlling the same wire)
+#define B2_bar    11                 // the pin connected to the wire B- of the coil B (or to the H-bridge pin controlling the same wire)
+#define B2        12                 // the pin connected to the wire B of the coil B (or to the H-bridge pin controlling the same wire)
+// motor controld variables
 #define x        1000                // smaller values may make the motor produce more speed and less torque
 // Note: approximate distance is 6.2um/step for current motors
 
-/*
-   Serial Parsing instantiations
-   includes variables for storing data, holding parsed data, and new data input checks
-*/
+#define xdirectionpin A0
+#define ydirectionpin A1
 
-const byte numChars = 32;
-char receivedChars[numChars];
-char tempChars[numChars];            // temporary array for use when parsing
-
-char stepDirection[numChars] = {0};
 int steps = 0;
+float joystick_speed=0.5;
+// Note: Change joystick speed as per your like. This is the ratio of movement of motors to movement of joystick. 
+//So, the higher the ratio the faster the motor moves.
 
-boolean newData = false;
+
+int updownstroke=0;
+int leftrightstroke=0;
+int upperlimitswitch=0;
+int lowerlimitswitch=0;
 
 /*
    Limit switch pin/interrupt instantiations
    Note: these pins vary depending on the board used. Validate at
    https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
 */
-byte up = 18;
-byte down = 19;
-byte left = 20;
-byte right = 21;
+#define up 7
+#define down 13 
+#define right 5
+#define left 6
 
-bool upTrigger = 0;
-bool downTrigger = 0;
-bool leftTrigger = 0;
-bool rightTrigger = 0;
+volatile bool upTrigger = 0;
+volatile bool downTrigger = 0;
+volatile bool leftTrigger = 0;
+volatile bool rightTrigger = 0;
 
 //======== For limit switch interrupts ==========//
+
+
 void limitUp() {
   if (digitalRead(up) == HIGH) {
     upTrigger = 0;
-    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Upper limit switch detects no collision");
+
   } else {
+    steps=200;
+    StepDown();
     upTrigger = 1;
-    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Upper limit switch detects collision");
+    
   }
 }
 
@@ -68,9 +75,12 @@ void limitDown() {
   if (digitalRead(down) == HIGH) {
     downTrigger = 0;
     digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Down limit switch detects no collision");
   } else {
+    steps=200;
+    StepUp();
     downTrigger = 1;
-    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Down limit switch detects collision");
   }
 }
 
@@ -78,9 +88,13 @@ void limitLeft() {
   if (digitalRead(left) == HIGH) {
     leftTrigger = 0;
     digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Left limit switch detects no collision");
   } else {
+    steps=200;
+    StepRight();
     leftTrigger = 1;
     digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Left limit switch detects collision");
   }
 }
 
@@ -88,56 +102,14 @@ void limitRight() {
   if (digitalRead(right) == HIGH) {
     rightTrigger = 0;
     digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Right limit switch detects no collision");
   } else {
     rightTrigger = 1;
+    steps=200;
+    StepLeft();
     digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Right limit switch detects collision");
   }
-}
-
-//========== For detecting start/end markers in Serial input ===========//
-void recvWithStartEndMarkers() {
-  static boolean recvInProgress = false;
-  static byte ndx = 0;
-  char startMarker = '<';
-  char endMarker = '>';
-  char rc;
-
-  while (Serial.available() > 0 && newData == false) {
-    rc = Serial.read();
-    if (recvInProgress == true) {
-      if (rc != endMarker) {
-        receivedChars[ndx] = rc;
-        ndx++;
-        if (ndx >= numChars) {
-          ndx = numChars - 1;
-        }
-      } else {
-        receivedChars[ndx] = '\0'; // terminate the string
-        recvInProgress = false;
-        ndx = 0;
-        newData = true;
-      }
-    } else if (rc == startMarker) {
-      recvInProgress = true;
-    }
-  }
-}
-
-//=========== For parsing Serial input data ============//
-void parseData() {      // split the data into its parts
-  char * strtokIndx; // this is used by strtok() as an index
-  strtokIndx = strtok(tempChars, ",");
-  strcpy(stepDirection, strtokIndx);
-  strtokIndx = strtok(NULL, ",");
-  steps = atoi(strtokIndx);
-}
-
-//============= To show parsed Serial inputs ==============//
-void showParsedData() {
-  Serial.print("Direction ");
-  Serial.println(stepDirection);
-  Serial.print("Steps ");
-  Serial.println(steps);
 }
 
 
@@ -145,17 +117,17 @@ void showParsedData() {
 //============ for motor control ================//
 
 void MotorsOn1() { // turns on motors, to start operating
-  pinMode(A1, OUTPUT);
-  pinMode(A1_bar, OUTPUT);
+  pinMode(A1motor, OUTPUT);
+  pinMode(A1motor_bar, OUTPUT);
   pinMode(B1, OUTPUT);
   pinMode(B1_bar, OUTPUT);
 }
 
 void MotorsOff1() { // turns off motors, to stop current draw. Useful after moving, or when not operating
-  pinMode(A1, INPUT);
-  digitalWrite(A1, HIGH);
-  pinMode(A1_bar, INPUT);
-  digitalWrite(A1_bar, HIGH);
+  pinMode(A1motor, INPUT);
+  digitalWrite(A1motor, HIGH);
+  pinMode(A1motor_bar, INPUT);
+  digitalWrite(A1motor_bar, HIGH);
   pinMode(B1, INPUT);
   digitalWrite(B1, HIGH);
   pinMode(B1_bar, INPUT);
@@ -163,17 +135,17 @@ void MotorsOff1() { // turns off motors, to stop current draw. Useful after movi
 }
 
 void MotorsOn2() { // turns on motors, to start operating
-  pinMode(A2, OUTPUT);
-  pinMode(A2_bar, OUTPUT);
+  pinMode(A2motor, OUTPUT);
+  pinMode(A2motor_bar, OUTPUT);
   pinMode(B2, OUTPUT);
   pinMode(B2_bar, OUTPUT);
 }
 
 void MotorsOff2() { // turns off motors, to stop current draw. Useful after moving, or when not operating
-  pinMode(A2, INPUT);
-  digitalWrite(A2, HIGH);
-  pinMode(A2_bar, INPUT);
-  digitalWrite(A2_bar, HIGH);
+  pinMode(A2motor, INPUT);
+  digitalWrite(A2motor, HIGH);
+  pinMode(A2motor_bar, INPUT);
+  digitalWrite(A2motor_bar, HIGH);
   pinMode(B2, INPUT);
   digitalWrite(B2, HIGH);
   pinMode(B2_bar, INPUT);
@@ -182,13 +154,15 @@ void MotorsOff2() { // turns off motors, to stop current draw. Useful after movi
 
 void StepUp() {
   MotorsOn1();
+
   for (int i = 0; i < (steps / 4) ; i++) {
     if (upTrigger) {
       Serial.println("Upper limit switch detects collision");
+      
       break;
     }
-    digitalWrite(A1, HIGH);
-    digitalWrite(A1_bar, LOW);
+    digitalWrite(A1motor, HIGH);
+    digitalWrite(A1motor_bar, LOW);
     digitalWrite(B1, HIGH);
     digitalWrite(B1_bar, LOW);
     delayMicroseconds (x);
@@ -197,8 +171,8 @@ void StepUp() {
       Serial.println("Upper limit switch detects collision");
       break;
     }
-    digitalWrite(A1, LOW);
-    digitalWrite(A1_bar, HIGH);
+    digitalWrite(A1motor, LOW);
+    digitalWrite(A1motor_bar, HIGH);
     digitalWrite(B1, HIGH);
     digitalWrite(B1_bar, LOW);
     delayMicroseconds (x);
@@ -207,8 +181,8 @@ void StepUp() {
       Serial.println("Upper limit switch detects collision");
       break;
     }
-    digitalWrite(A1, LOW);
-    digitalWrite(A1_bar, HIGH);
+    digitalWrite(A1motor, LOW);
+    digitalWrite(A1motor_bar, HIGH);
     digitalWrite(B1, LOW);
     digitalWrite(B1_bar, HIGH);
     delayMicroseconds (x);
@@ -217,13 +191,14 @@ void StepUp() {
       Serial.println("Upper limit switch detects collision");
       break;
     }
-    digitalWrite(A1, HIGH);
-    digitalWrite(A1_bar, LOW);
+    digitalWrite(A1motor, HIGH);
+    digitalWrite(A1motor_bar, LOW);
     digitalWrite(B1, LOW);
     digitalWrite(B1_bar, HIGH);
     delayMicroseconds (x);
   }
   Serial.println("StepUp Subroutine Complete");
+  upTrigger=0;
   MotorsOff1();
 }
 
@@ -232,45 +207,50 @@ void StepDown() {
   for (int i = 0; i < (steps / 4); i++) {
     if (downTrigger) {
       Serial.println("Lower limit switch detects collision");
+      
       break;
     }
-    digitalWrite(A1, HIGH);
-    digitalWrite(A1_bar, LOW);
+    digitalWrite(A1motor, HIGH);
+    digitalWrite(A1motor_bar, LOW);
     digitalWrite(B1, LOW);
     digitalWrite(B1_bar, HIGH);
     delayMicroseconds (x);
 
     if (downTrigger) {
       Serial.println("Lower limit switch detects collision");
+    
       break;
     }
-    digitalWrite(A1, LOW);
-    digitalWrite(A1_bar, HIGH);
+    digitalWrite(A1motor, LOW);
+    digitalWrite(A1motor_bar, HIGH);
     digitalWrite(B1, LOW);
     digitalWrite(B1_bar, HIGH);
     delayMicroseconds (x);
 
     if (downTrigger) {
       Serial.println("Lower limit switch detects collision");
+      
       break;
     }
-    digitalWrite(A1, LOW);
-    digitalWrite(A1_bar, HIGH);
+    digitalWrite(A1motor, LOW);
+    digitalWrite(A1motor_bar, HIGH);
     digitalWrite(B1, HIGH);
     digitalWrite(B1_bar, LOW);
     delayMicroseconds (x);
 
     if (downTrigger) {
       Serial.println("Lower limit switch detects collision");
+      
       break;
     }
-    digitalWrite(A1, HIGH);
-    digitalWrite(A1_bar, LOW);
+    digitalWrite(A1motor, HIGH);
+    digitalWrite(A1motor_bar, LOW);
     digitalWrite(B1, HIGH);
     digitalWrite(B1_bar, LOW);
     delayMicroseconds (x);
   }
   Serial.println("StepDown Subroutine Complete");
+  downTrigger=0;
   MotorsOff1();
 }
 
@@ -279,10 +259,11 @@ void StepLeft() {
   for (int i = 0; i < (steps / 4) ; i++) {
     if (leftTrigger) {
       Serial.println("Left limit switch detects collision");
+
       break;
     }
-    digitalWrite(A2, HIGH);
-    digitalWrite(A2_bar, LOW);
+    digitalWrite(A2motor, HIGH);
+    digitalWrite(A2motor_bar, LOW);
     digitalWrite(B2, HIGH);
     digitalWrite(B2_bar, LOW);
     delayMicroseconds (x);
@@ -291,8 +272,8 @@ void StepLeft() {
       Serial.println("Left limit switch detects collision");
       break;
     }
-    digitalWrite(A2, LOW);
-    digitalWrite(A2_bar, HIGH);
+    digitalWrite(A2motor, LOW);
+    digitalWrite(A2motor_bar, HIGH);
     digitalWrite(B2, HIGH);
     digitalWrite(B2_bar, LOW);
     delayMicroseconds (x);
@@ -301,8 +282,8 @@ void StepLeft() {
       Serial.println("Left limit switch detects collision");
       break;
     }
-    digitalWrite(A2, LOW);
-    digitalWrite(A2_bar, HIGH);
+    digitalWrite(A2motor, LOW);
+    digitalWrite(A2motor_bar, HIGH);
     digitalWrite(B2, LOW);
     digitalWrite(B2_bar, HIGH);
     delayMicroseconds (x);
@@ -311,13 +292,14 @@ void StepLeft() {
       Serial.println("Left limit switch detects collision");
       break;
     }
-    digitalWrite(A2, HIGH);
-    digitalWrite(A2_bar, LOW);
+    digitalWrite(A2motor, HIGH);
+    digitalWrite(A2motor_bar, LOW);
     digitalWrite(B2, LOW);
     digitalWrite(B2_bar, HIGH);
     delayMicroseconds (x);
   }
   Serial.println("StepLeft Subroutine Complete");
+  leftTrigger=0;
   MotorsOff2();
 }
 
@@ -328,8 +310,8 @@ void StepRight() {
       Serial.println("Right limit switch detects collision");
       break;
     }
-    digitalWrite(A2, HIGH);
-    digitalWrite(A2_bar, LOW);
+    digitalWrite(A2motor, HIGH);
+    digitalWrite(A2motor_bar, LOW);
     digitalWrite(B2, LOW);
     digitalWrite(B2_bar, HIGH);
     delayMicroseconds (x);
@@ -338,8 +320,8 @@ void StepRight() {
       Serial.println("Right limit switch detects collision");
       break;
     }
-    digitalWrite(A2, LOW);
-    digitalWrite(A2_bar, HIGH);
+    digitalWrite(A2motor, LOW);
+    digitalWrite(A2motor_bar, HIGH);
     digitalWrite(B2, LOW);
     digitalWrite(B2_bar, HIGH);
     delayMicroseconds (x);
@@ -348,8 +330,8 @@ void StepRight() {
       Serial.println("Right limit switch detects collision");
       break;
     }
-    digitalWrite(A2, LOW);
-    digitalWrite(A2_bar, HIGH);
+    digitalWrite(A2motor, LOW);
+    digitalWrite(A2motor_bar, HIGH);
     digitalWrite(B2, HIGH);
     digitalWrite(B2_bar, LOW);
     delayMicroseconds (x);
@@ -358,15 +340,18 @@ void StepRight() {
       Serial.println("Right limit switch detects collision");
       break;
     }
-    digitalWrite(A2, HIGH);
-    digitalWrite(A2_bar, LOW);
+    digitalWrite(A2motor, HIGH);
+    digitalWrite(A2motor_bar, LOW);
     digitalWrite(B2, HIGH);
     digitalWrite(B2_bar, LOW);
     delayMicroseconds (x);
   }
   Serial.println("StepRight Subroutine Complete");
+  rightTrigger=0;
   MotorsOff2();
 }
+
+
 
 //=================== Arduino Setup ===================//
 void setup() {
@@ -376,52 +361,82 @@ void setup() {
 
   // For Serial Monitor
   Serial.begin(9600);
-  Serial.println("Enter commands in the form of <stepDirection,steps>");
-  Serial.println("valid stepDirections are {up,down,left,right}");
-  Serial.println();
+  Serial.println("The setup is ready. Use joystick for movement.");
 
-  // For limit switch interrupts
-  digitalWrite(up,    HIGH);
-  digitalWrite(down,  HIGH);
-  digitalWrite(left,  HIGH);
-  digitalWrite(right, HIGH);
+  pinMode(up, INPUT_PULLUP);
+  pinMode(down, INPUT_PULLUP);
+  pinMode(right, INPUT_PULLUP);
+  pinMode(left, INPUT_PULLUP);
 
-  // These require different syntax for arduino uno rev2 wifi, differ per board. Check:
-  // https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
-  attachInterrupt(digitalPinToInterrupt(up), limitUp,    CHANGE);
-  attachInterrupt(digitalPinToInterrupt(down), limitDown,  CHANGE);
-  attachInterrupt(digitalPinToInterrupt(left), limitLeft,  CHANGE);
-  attachInterrupt(digitalPinToInterrupt(right), limitRight, CHANGE);
+  if(updownstroke==0){
+    Serial.println("Homing starting");
 
-  // Built-in LED to flash when we hit any of our limit switches
-  pinMode(LED_BUILTIN, OUTPUT);
+     while(digitalRead(up) == HIGH){
+        steps=10;
+        StepUp();  
+     }
+
+    while(digitalRead(down) == HIGH){
+      steps=10;
+      StepDown();
+      updownstroke=updownstroke+steps;
+    }
+    while(digitalRead(right) == HIGH){
+        steps=20;
+        StepRight();  
+     }
+
+    while(digitalRead(left) == HIGH){
+      steps=10;
+      StepLeft();
+      leftrightstroke=leftrightstroke+steps;
+    }
+    
+  }
+  Serial.println("updownstroke");
+  Serial.println(updownstroke);
+  Serial.println("leftrightstroke");
+  Serial.println(leftrightstroke);
+  steps=updownstroke/2;
+  StepUp();
+  steps=leftrightstroke/2;
+  StepRight();
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(up), limitUp, FALLING);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(down), limitDown, FALLING);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(right), limitRight, FALLING);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(left), limitLeft, FALLING);
+
+  
+
 }
 
 //============= Main loop ================//
 void loop() {
-  // Scrape for data from Serial Monitor
-  recvWithStartEndMarkers();
-  // If new input, begin subroutine
-  if (newData == true) {
-    strcpy(tempChars, receivedChars);
-    // this temporary copy is necessary to protect the original data
-    //   because strtok() used in parseData() replaces the commas with \0
-    parseData();
-    showParsedData();
-    newData = false; // ensures we don't have infinite loop
+  // read data from joystick and use that for movement.
+  
+   int xdirection=analogRead(xdirectionpin);
+   int ydirection=analogRead(ydirectionpin);
+  
+    int xdirection_difference= xdirection-525;
+    int ydirection_difference= ydirection-512;
 
-    // Comparing input to determine direction of operation (if any) and instigating operation
-    if (strcmp(stepDirection, "up") == 0) {
+    if (ydirection_difference < -5) {
+      steps=joystick_speed*abs(ydirection_difference);
+      Serial.println(steps);
       StepUp();
     }
-    if (strcmp(stepDirection, "down") == 0) {
+    if (ydirection_difference >5 ) {
+      steps=joystick_speed*abs(ydirection_difference);
+      Serial.println(steps);
       StepDown();
     }
-    if (strcmp(stepDirection, "left") == 0) {
-      StepLeft();
-    }
-    if (strcmp(stepDirection, "right") == 0) {
+    if (xdirection_difference < -5) {
+      steps=joystick_speed*abs(xdirection_difference);
       StepRight();
     }
-  }
+    if (xdirection_difference > 5) {
+      steps=joystick_speed*abs(xdirection_difference);
+      StepLeft();
+    }
+  
 }
